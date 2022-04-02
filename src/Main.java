b93bd6963bd9f370b215b78d4824d99f918bcaec
EscapeFromJava <reske.artur@gmail.com>
2022-04-02 19:51:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите общее кол-во деревьев");
+        int n = scan.nextInt();
+        System.out.println("Введите кол-во деревьев, которое должно остаться");
+        int m = scan.nextInt();
+        int space = 1;
+        int count = n - m + 1;
+        while(m + (space * (m - 1)) - 1 <= n){
+            if (m + (space * (m - 1)) - 1 <= n)
+                count += n - (m + (space * (m - 1))) + 1;
+            space++;
+        }
+        System.out.println("Кол-во способов: " + count);
+    }
+}
